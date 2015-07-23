b9f0d9fb3053e087287c356967586041e9578edc
Sahar Jafari <shr.jafari@gmail.com>
2015-07-22 21:10:27 -0700

    add color pallet and center the result

--- js/bmi.js
+++ js/bmi.js
@@ -1,6 +1,21 @@
 'use strict';
 var app = angular.module('BMI', ['ngMaterial']);
 
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('teal', {
+      'default': '800', // by default use shade 400 from the pink palette for primary intentions
+      'hue-1': '500', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': 'A700', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('orange', {
+      'default': '500' // use shade 200 for default, and keep all other shades the same
+    });
+ });
+
 app.controller('BMICtrl', function($scope){
   $scope.data = {
       selectedIndex: 0,
