3ba098995982a40c72e754f6281be6fb38d18db9
Sahar Jafari <shr.jafari@gmail.com>
2015-07-24 19:18:47 -0700

    add desired bmi and weight

--- js/bmi.js
+++ js/bmi.js
@@ -9,60 +9,117 @@ app.config(function($mdThemingProvider) {
       'hue-2': 'A700', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
     })
+    // If you specify less than all of the keys, it will inherit from the
+    // default shades
+    .accentPalette('orange', {
+      'default': '500' // use shade 200 for default, and keep all other shades the same
+    });
  });
 
 app.controller('BMICtrl', function($scope){
+  
+  $scope.tabs = {
+  	weight: 0,
+  	height: 0
+   };
+
   $scope.data = {
-      selectedIndex: 0,
-      bottom: false
-    };
-
-  $scope.next = function() {
-      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+  	input: {
+  		metric: {
+  			weight: '',
+  			height: ''
+  		},
+  		imperial: {
+  			weight: '',
+  			height: {
+  				foot: '',
+  				inch: ''
+  			}
+  		}
+  	},
+  	output: {
+  		bmi: 0,
+  		status: 0,
+  		show: false
+  	},
+  	desired: {
+  		bmi: 0,
+  		weight: 0,
+  		weightUnit: 'kg'
+  	}
   };
-  $scope.previous = function() {
-      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
-  };
-  
-  
 
-  $scope.calculate = function(){
-  	var w = $scope.weight;
-  	var h = $scope.heightM;
+  $scope.STATUS = [
+  	'Underweight',
+  	'Normal',
+  	'Overweight',
+  	'Obese',
+  	'Morbid Obese'
+  ];
 
-  	if(!w || !h) {
-  		return 0;
+  $scope.$watch('data.input', function(){
+  	var weight;
+  	var height;
+
+  	if($scope.tabs.weight === 0) {
+  		weight = $scope.data.input.metric.weight;
+  	}
+  	else {
+  		weight = $scope.data.input.imperial.weight;
+  		weight = weight / 2.2046;
+  	}
+  	if($scope.tabs.height === 0) {
+  		height = $scope.data.input.metric.height;
+  	}
+  	else {
+  		var foot = $scope.data.input.imperial.height.foot;
+  		var inch = $scope.data.input.imperial.height.inch;
+  		height = ((foot * 12) + inch) * 0.0254;
   	}
 
-  	if($scope.weightTab === 2) {
-  		w = w / 2.2046;
+  	height = Math.pow(height, 2);
+  	$scope.data.output.bmi = weight / height;
+
+  	var bmi = $scope.data.output.bmi;
+  	$scope.data.desired.bmi = bmi;
+
+  	if(isNaN(bmi) || !Number.isFinite(bmi)) {
+  		$scope.data.output.show = false;
+  	} else {
+  		$scope.data.output.show = true;
   	}
-  	if($scope.heightTab === 4){
-  		var inch = $scope.heightI;
-  		h = ((h * 12) + inch) * 0.0254;
+
+  	if(bmi < 18.5) {
+  		$scope.data.output.status = 0;
   	}
+    else if(bmi >= 18.5 && bmi <=24.9) {
+		$scope.data.output.status = 1;
+    }
+    else if(bmi >= 25 && bmi <= 29.9) {
+        $scope.data.output.status = 2;
+    }
+    else if (bmi >= 30 && bmi <= 35) {
+        $scope.data.output.status = 3;
+    }
+    else {
+    	$scope.data.output.status = 4;
+    }
 
-    h = Math.pow(h, 2);
-    return w / h;
-  };
+  }, true);
 
-  $scope.showTxt = function() {
-	var bmi = $scope.calculate();
+	$scope.$watch('data.desired.bmi', function(){
+		var weight = 100;
 
-	if(bmi < 18.5) {
-		return 'Underweight';
-	}
-	if(bmi >= 18.5 && bmi <=24.9) {
-		return 'Normal';
-	}
-	if(bmi >= 25 && bmi <= 29.9) {
-		return 'Overweight';
-	}
-	if (bmi >= 30) {
-		return 'Obese';
-	}
-	else {
-		return 'Input Error!';
-	}
-  };
+		$scope.data.desired.weight = weight;
+		
+		if($scope.tabs.weight === 0) {
+			$scope.data.desired.weightUnit = 'kg';
+  		} else {
+			$scope.data.desired.weightUnit = 'lbs';
+  		}
+		// var bmi = $scope.data.desired.bmi;
+		// var height = 100;
+		// weight = bmi * height;
+	});
+
 });
