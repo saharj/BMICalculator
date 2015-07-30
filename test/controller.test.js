863bcb37e50e9816e6321b5f033c3d2109640c5b
Sahar Jafari <shr.jafari@gmail.com>
2015-07-29 23:12:45 -0700

    delet my.config.js file

--- test/controller.test.js
+++ test/controller.test.js
@@ -11,6 +11,7 @@ describe('BMIcalc', function() {
         });
 	});
 
+
 	describe('check $scope.data', function() {
 		it('output.show should be false', function() {
 			expect($scope.data.output.show).toBe(false);
@@ -20,8 +21,17 @@ describe('BMIcalc', function() {
 		});
 	})
 
+	describe('the BMI', function() {
+		it('BMI for metric measures', function() {
+			$scope.data.input.metric.weight = 57.4;
+			$scope.data.input.metric.height = 164.5;
+			$scope.$digest();
+			expect($scope.data.output.bmi).toBeCloseTo(21.2, 1);
+		});
+	});
+
 	describe('$scope.STATUS', function() {
-		it('Underweight should be grey color', function() {
+		it('Overweight should be gold color', function() {
 			$scope.data.output.status = 2;
 			expect($scope.STATUS[$scope.data.output.status]).toBe('Overweight');
 		});
